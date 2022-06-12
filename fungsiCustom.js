b040e65224ef8d121720361ef6382c861af404a3
dewisapp <dew.fitri2@gmail.com>
2022-06-12 11:36:58 +0700

    isi soal jawaban

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let allData = [];
+  fs.readFile(file1, "utf8", (err, data1) => {
+    if (err) {
+      return console.log("Terjadi error pada data 1" + err);
+    } else {
+      const fileData1 = JSON.parse(data1);
+      allData.push(fileData1.message.split(" ")[1]);
+    }
+    fs.readFile(file2, "utf8", (err, data2) => {
+      if (err) {
+        return console.log("Terjadi error pada data 2" + err);
+      } else {
+        const fileData2 = JSON.parse(data2);
+        allData.push(fileData2[0].message.split(" ")[1]);
+      }
+      fs.readFile(file3, "utf8", (err, data3) => {
+        if (err) {
+          return console.log("Terjadi error pada data 3" + err);
+        } else {
+          const fileData3 = JSON.parse(data3);
+          allData.push(fileData3[0].data.message.split(" ")[1]);
+        }
+
+        console.log(allData);
+        fnCallback(err, allData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
